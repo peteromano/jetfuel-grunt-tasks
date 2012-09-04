8f155b201502a3ededfbb84deef51224ad5c5b2c
Pete Romano <pete.romano@yahoo.com>
2012-09-04 19:36:32 -0400

    Finished first version of espresso plugin architecture

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,20 +1,28 @@
 module.exports = function(grunt) {
     'use strict';
 
+    var ESPRESSO_JSON = '.espresso.json';
+
     grunt.registerMultiTask('copy', 'Copy src files to dest directory.', function() {
-        var dest = this.file.dest;
+        var path = require('path'),
+            fs = require('fs'),
+            dest = this.file.dest,
+            vendor, lib;
 
         try {
-
             grunt.file.expandFiles(this.file.src).forEach(function(filepath){
-              grunt.file.copy(filepath, grunt.helper('path.to_dest_path', filepath, dest));
+              if(path.basename(filepath) != ESPRESSO_JSON) grunt.file.copy(filepath, grunt.helper('path.to_dest_path', filepath, dest));
+              else {
+                  vendor = (grunt.file.readJSON(filepath) || {}).vendor;
+                  lib = vendor.lib;
+                  for(var file in lib) {
+                      grunt.file.copy(path.dirname(filepath) + '/' + file, path.dirname(grunt.helper('path.to_dest_path', filepath, dest)) + '/' + path.basename(lib[file]));
+                  }
+              }
             });
-
         } catch(e) {
-
             grunt.log.writeln(e);
             return false;
-
         }
     });
 
