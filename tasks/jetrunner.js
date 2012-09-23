4077db8846877373b7f5aaffb737cc3058bb8ca2
Pete Romano <pete.romano@yahoo.com>
2012-09-22 21:25:57 -0400

    Fix hook

--- tasks/jetrunner.js
+++ tasks/jetrunner.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.registerMultiTask('jetrunner', 'Unit test server.', function() {
-        require('jetfuel.test.jetrunner').server.start();
+        require('jetfuel.test.jetrunner').server('start');
     });
 
 };
