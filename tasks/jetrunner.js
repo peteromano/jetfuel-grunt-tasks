45f1a71131fb6c2166754c085c48713d25fdbee1
Pete Romano <pete.romano@yahoo.com>
2012-09-24 18:50:54 -0400

    Light documentation for the jetrunner hooks

--- tasks/jetrunner.js
+++ tasks/jetrunner.js
@@ -2,7 +2,22 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.registerMultiTask('jetrunner', 'Unit test server.', function() {
-        require('jetfuel.test.jetrunner').server('start');
+        var data = this.data;
+
+        require('jetfuel.test.jetrunner')
+
+            // Configure and start Express server
+            .server('start', {
+                src: data.src,
+                test: data.test,
+                vendor: data.vendor,
+                runner: data.runner,
+                server: data.server,
+                soda: data.soda
+            })
+
+            // Enumerate and run tests asynchronously from filenames in test directory
+            .test([]);
     });
 
 };
