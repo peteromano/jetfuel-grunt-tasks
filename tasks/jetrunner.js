623b87050227b017197a5c78d50ef1aef13e4530
Pete Romano <pete.romano@yahoo.com>
2012-09-27 18:21:50 -0400

    Finished jetrunner config signature

--- tasks/jetrunner.js
+++ tasks/jetrunner.js
@@ -2,20 +2,29 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.registerMultiTask('jetrunner', 'Unit test server.', function() {
-        this.requiresConfig('meta.jetrunner');
+        var d = this.data, data;
 
-        var data = grunt.utils._.extend(grunt.config.process('meta.jetrunner') || {}, this.data || {});
+        for(var test in d) {
+            data = d[test];
 
-        require('jetfuel.test.jetrunner')
-
-            // Configure and start JetRunner server
-            .server('start', data.server)
-
-            // Run unit tests
-            .run(grunt.file.expandFiles(data.test), data.src, {
-                runner: data.runner,
-                remote: data.remote
+            data = grunt.utils.recurse(data, function(value) {
+                value = grunt.task.directive(value);
+                return typeof value === 'string' && grunt.template.process(value, grunt.config()) || value;
             });
+
+            require('jetfuel.test.jetrunner')
+
+                // Configure and start JetRunner server
+                .server('start', data.server)
+
+                // Run unit tests
+                .run(grunt.file.expandFiles(data.test), data.src, {
+                    runner: data.runner,
+                    reporter: data.reporter,
+                    remote: data.remote
+                });
+
+        }
     });
 
 };
