3238eecf60669e7fe581a7fd9adaa56c5728f0d3
Pete Romano <pete.romano@yahoo.com>
2012-09-28 16:48:16 -0400

    Fixed autocat bugs

--- tasks/autocat.js
+++ tasks/autocat.js
@@ -5,14 +5,14 @@ module.exports = function(grunt) {
         var dest = this.file.dest,
             banner = grunt.task.directive(this.data.banner) || '',
             filters = this.data.filters || ['*.js'],
-            separator = this.data.separator || ';',
+            separator = typeof this.data.separator !== undefined ? this.data.separator : ';',
             extension = this.data.extension || '.js',
             verbose = true,//this.data.verbose,
             src = this.file.src;
 
         try {
             for(var fileglob in src) {
-                grunt.file.expandDirs(fileglob = src[fileglob]).forEach(function(dirpath){
+                grunt.file.expandDirs(fileglob = src[fileglob]).reverse().forEach(function(dirpath){
                     var newFile;
 
                     grunt.file.write(
