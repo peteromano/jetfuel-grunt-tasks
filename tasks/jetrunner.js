024e0c0a88a1e30eac9e0a5f5e7a04d02d734bcc
Pete Romano <pete.romano@yahoo.com>
2012-09-28 15:47:02 -0400

    Getting the JetRunner task more solid

--- tasks/jetrunner.js
+++ tasks/jetrunner.js
@@ -2,26 +2,31 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.registerMultiTask('jetrunner', 'Unit test server.', function() {
-        var d = this.data, data;
+        var process = grunt.template.process,
+            data = this.data,
+            tests, target;
 
-        for(var test in d) {
-            data = d[test];
+        for(var test in data) {
+            target = data[test];
+            tests = {};
 
-            data = grunt.utils.recurse(data, function(value) {
+            target = grunt.utils.recurse(target, function(value) {
                 value = grunt.task.directive(value);
-                return typeof value === 'string' && grunt.template.process(value, grunt.config()) || value;
+                return typeof value === 'string' && process(value, grunt.config()) || value;
             });
+
+            for(var t in target.tests) tests[process(t)] = process(target.tests[t]);
 
             require('jetfuel.test.jetrunner')
 
                 // Configure and start JetRunner server
-                .server('start', data.server)
+                .server('start', target.server)
 
                 // Run unit tests
-                .run(grunt.file.expandFiles(data.tests), {
-                    runner: data.runner,
-                    reporter: data.reporter,
-                    remote: data.remote
+                .run(tests, {
+                    runner: target.runner,
+                    reporter: target.reporter,
+                    remote: target.remote
                 });
 
         }
